92dc5d30bf05f1aa81434d1efe2a78e32c1cd241
Susanakillswitch <susy_spedy@hotmail.com>
2023-11-08 18:58:19 -0700

    tarea 6

--- ejercicio6-17.py
+++ ejercicio6-17.py
@@ -17,20 +17,20 @@ y = np.sqrt(delta_t *(alpha - alpha ** 2)) * prng.standard_normal(n_points - 1) 
 w_ = np.roll(w, -1)  # w_i+1, recorer las casillas, el -1 significa que te recorre el del final al primero 
 
 w_alpha = alpha * w_ + (1 - alpha) * w # es un vector que tiene como elementos w_{i+\alpha}
-w_alpha = np.delete(w_alpha, -1) # estamos elimando eñ ultimo que no es un incremento del vector W_{\alpha}
-w_alpha += y # una manera 
-w_ref = np.zeros(2* n_points -1)
+w_alpha = np.delete(w_alpha, -1) # estamos elimando el ultimo que no es un incremento del vector W_{\alpha}
+w_alpha += y # una forma para sumar vectores, para completar el ajuste
+w_ref = np.zeros(2* n_points -1) # definiendo otro vector browniano completo (refinado)
 
-w_ref[0::2] = w
+w_ref[0::2] = w # rellenamos con los elemntos de w en ods pasos
 w_ref[1::2] = w_alpha
 
-time_ref = np.zeros(2 * n_points - 1)
+time_ref = np.zeros(2 * n_points - 1) # particiones refinadas
 
 for i in range(2 * n_points - 1):
     if i % 2 == 0:
-        time_ref[i] = time[int(i / 2)]
+        time_ref[i] = time[int(i / 2)] # para que se conserven las particiones originales
     else:
-        time_ref[i] = time[int(i / 2)] + alpha * delta_t
+        time_ref[i] = time[int(i / 2)] + alpha * delta_t # para agregar las particiones refinadas
 
 plt.plot(time_ref,w_ref,'g-')
 plt.plot(time, w,'ro')
